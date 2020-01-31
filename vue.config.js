f3a1f5495d86fae75c9351817332aa7f01bd24c3
xdx <2717814812@qq.com>
2020-01-31 21:34:23 +0800

    remove mock

--- vue.config.js
+++ vue.config.js
@@ -36,6 +36,18 @@ module.exports = {
       warnings: false,
       errors: true
     },
+    proxy: {
+      // change xxx-api/login => mock/login
+      // detail: https://cli.vuejs.org/config/#devserver-proxy
+      [process.env.VUE_APP_BASE_API]: {
+        //target: `http://127.0.0.1:${port}/mock`,
+        target: `http://localhost:8080`,
+        changeOrigin: true,
+        pathRewrite: {
+          ['^' + process.env.VUE_APP_BASE_API]: ''
+        }
+      }
+    },
     before: require('./mock/mock-server.js')
   },
   configureWebpack: {
